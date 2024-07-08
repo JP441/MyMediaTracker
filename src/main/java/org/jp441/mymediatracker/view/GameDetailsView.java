69b0c4ee119d033d084f2d0c657d5366ca670635
JP441 <jermaine4411@gmail.com>
2024-07-08 18:04:09 +1000

    feat(game-details): add genres to game details display

--- src/main/java/org/jp441/mymediatracker/view/GameDetailsView.java
+++ src/main/java/org/jp441/mymediatracker/view/GameDetailsView.java
@@ -9,6 +9,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import org.jp441.mymediatracker.Game;
 
 public class GameDetailsView {
@@ -26,6 +27,7 @@ public class GameDetailsView {
         VBox root = new VBox();
         root.getChildren().add(createHeaderHBox());
         root.getChildren().add(createGameTitleHBox());
+        root.getChildren().add(createGenresHBox());
         root.getChildren().add(backBtn);
         Scene scene = new Scene(root, 1226, 878);
         scene.getStylesheets().add(getClass().getResource("/org/jp441/mymediatracker/css/darkMode.css").toExternalForm());
@@ -70,6 +72,33 @@ public class GameDetailsView {
         hbox.setAlignment(Pos.CENTER);
         hbox.setId("GameTitleHBox");
         return hbox;
+    }
+
+    private HBox createGenresHBox() {
+        HBox tagHBox = new HBox(5);
+        tagHBox.setPadding(new Insets(10,0,10,0));
+        if(!game.getGenres().isEmpty()) {
+            for(String genre: game.getGenres()) {
+                StackPane genreTag = createGenreTag(genre);
+                tagHBox.getChildren().add(genreTag);
+            }
+        }
+        return tagHBox;
+    }
+
+    private StackPane createGenreTag(String genre) {
+        Label label = new Label(genre);
+        label.setPadding(new Insets(5,10,5,10));
+        Rectangle rectangle = new Rectangle();
+        rectangle.setArcWidth(30);
+        rectangle.setArcHeight(30);
+        rectangle.setFill(Color.WHITE);
+        rectangle.setStroke(Color.BLACK);
+        rectangle.widthProperty().bind(label.widthProperty().add(20));
+        rectangle.heightProperty().bind(label.heightProperty().add(10));
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().addAll(rectangle, label);
+        return stackPane;
     }
 
 
